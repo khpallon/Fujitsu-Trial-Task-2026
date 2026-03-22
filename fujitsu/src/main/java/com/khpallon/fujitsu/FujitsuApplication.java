aee0c4d202ed6a5f37bb5c9fc7dbdeba7c1d19a5
khpallon <karlhannespallon@gmail.com>
2026-03-22 14:50:16 +0200

    project start

---
+++ fujitsu/src/main/java/com/khpallon/fujitsu/FujitsuApplication.java
@@ -0,0 +1,13 @@
+package com.khpallon.fujitsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FujitsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FujitsuApplication.class, args);
+	}
+
+}
